21e4073720fb66134e9dd9e9513008890131d23d
David073471 <ds073471@gmail.com>
2025-12-11 17:00:50 -0300

    Poner url para añadir direccion a la zona de reportes

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -4,12 +4,11 @@ from . import views
 app_name = 'mainApp'
 
 urlpatterns = [
-    path('', views.catalogo, name='home'),
-    path('catalogo/', views.catalogo, name='catalogo'),
-    path('producto/<slug:slug>/', views.detalle_producto, name='detalle_producto'),
-    path('producto/<slug:slug>/solicitar/', views.crear_pedido, name='crear_pedido'),
-    path('seguimiento/', views.seguimiento, name='seguimiento'),  
-    path('pedido/pagar/<str:token>/', views.marcar_pago, name='marcar_pago'),
-    path('reporte/', views.reporte_sistema, name='reporte'),
-
+    path("", views.catalogo, name="catalogo"),
+    path("producto/<slug:slug>/", views.detalle_producto, name="detalle_producto"),
+    path("producto/<slug:slug>/solicitar/", views.crear_pedido, name="crear_pedido"),
+    path("crear-pedido/", views.crear_pedido, name="crear_pedido_simple"),
+    path("pago/<str:token>/", views.marcar_pago, name="marcar_pago"),
+    path("seguimiento/", views.seguimiento, name="seguimiento"),
+    path("reporte/", views.reporte_dashboard, name="reporte"),
 ]
